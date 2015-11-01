a7e1d1d1e843fad2f25f5e330b3371eecf8888b3
shrutiyer <shruti.iyer@students.olin.edu>
2015-11-01 18:40:37 -0500

    Making catkin_ws

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/shruti/POE/OCR/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/shruti/POE/OCR/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/shruti/POE/OCR/devel/lib:/home/shruti/POE/OCR/devel/lib/x86_64-linux-gnu:/home/shruti/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/shruti/catkin_ws/devel/lib:/opt/ros/indigo/lib:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PATH="/home/shruti/POE/OCR/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/shruti/POE/OCR/devel/lib/pkgconfig:/home/shruti/POE/OCR/devel/lib/x86_64-linux-gnu/pkgconfig:/home/shruti/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/shruti/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/shruti/POE/OCR/build"
+export PYTHONPATH="/home/shruti/POE/OCR/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/shruti/POE/OCR/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/shruti/POE/OCR/src:$ROS_PACKAGE_PATH"
